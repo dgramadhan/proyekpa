b24e8e2c4f9039756a59703fa21a08b0202b1d54
dgramadhan <62194600+dgramadhan@users.noreply.github.com>
2020-03-15 11:13:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 8080;
-//     bodyParser = require('body-parser'),
-//     controller = require('./controller');
+// var express = require('express'),
+//     app = express(),
+//     port = process.env.PORT || 8080;
+// //     bodyParser = require('body-parser'),
+// //     controller = require('./controller');
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// // kalau pengen ujicoba tanpa link comment ini dibawah
-// // app.use(express.static('public'));
-// // app.use(express.static('assets'));
-// // app.use(express.static('json'));
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// // app.use(bodyParser.json());
+// // // kalau pengen ujicoba tanpa link comment ini dibawah
+// // // app.use(express.static('public'));
+// // // app.use(express.static('assets'));
+// // // app.use(express.static('json'));
 
-// // 
+// // // 
 
-// var routes = require('./routes');
-// routes(app);
+// // var routes = require('./routes');
+// // routes(app);
 
-app.listen(port);
-console.log('API jalan di port : ' + port);
+// app.listen(port);
+// console.log('API jalan di port : ' + port);
+
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 4000app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));app.listen(PORT, () => console.log(`app listening on port ${PORT}`))

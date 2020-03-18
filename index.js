c4d6185279dfdf78c35add8d75504c167d962bc6
dgramadhan <62194600+dgramadhan@users.noreply.github.com>
2020-03-18 18:49:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express'),
 
 app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
 app.listen(port, () => console.log(`app listening on port ${port}`))
-app.get('/home', (req, res) => res.sendFile(path.join(__dirname, 'web.html')));
+app.get('/home', (req, res) => res.sendFile(path.join(__dirname, 'web_test.html')));
 
 // //     bodyParser = require('body-parser'),
 // //     controller = require('./controller');
